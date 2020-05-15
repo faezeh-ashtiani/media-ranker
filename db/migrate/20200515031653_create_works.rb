7b68a5bba2493be4bb5f818702d9edc6440c9c09
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-05-14 20:32:20 -0700

    created the DB, work model and schema. seeded some work

--- db/migrate/20200515031653_create_works.rb
+++ db/migrate/20200515031653_create_works.rb
@@ -1,10 +1,10 @@
 class CreateWorks < ActiveRecord::Migration[6.0]
   def change
     create_table :works do |t|
-      t.string :catagory
+      t.string :category
       t.string :title
       t.string :creator
-      t.integer :year
+      t.integer :publication_year
       t.string :description
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 
-WORK_FILE = Rails.root.join('db', 'works-seed.csv')
+WORK_FILE = Rails.root.join('db', 'works-seeds.csv')
 puts "Loading raw work data from #{WORK_FILE}"
 
 work_failures = []
 CSV.foreach(WORK_FILE, :headers => true) do |row|
   work = Work.new
-  work.id = row['id']
-  work.name = row['name']
-  work.vin = row['vin']
-  work.available = row['available']
+  work.category = row['category']
+  work.title = row['title']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year']
+  work.description = row['description']
   successful = work.save
   if !successful
     work_failures << work
@@ -30,29 +31,6 @@ puts "Added #{Work.count} work records"
 puts "#{work_failures.length} works failed to save"
 
 
-
-PASSENGER_FILE = Rails.root.join('db', 'seed_data', 'passengers.csv')
-puts "Loading raw passenger data from #{PASSENGER_FILE}"
-
-passenger_failures = []
-CSV.foreach(PASSENGER_FILE, :headers => true) do |row|
-  passenger = Passenger.new
-  passenger.category = row['category']
-  passenger.title = row['title']
-  passenger.creator = row['creator']
-  passenger.publication_year = row['publication_year']
-  passenger.description = row['description']
-  successful = passenger.save
-  if !successful
-    passenger_failures << passenger
-    puts "Failed to save passenger: #{passenger.inspect}"
-  else
-    puts "Created passenger: #{passenger.inspect}"
-  end
-end
-
-puts "Added #{Passenger.count} passenger records"
-puts "#{passenger_failures.length} passengers failed to save"
 
 # Since we set the primary key (the ID) manually on each of the
 # tables, we've got to tell postgres to reload the latest ID

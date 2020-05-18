86321a345e2ecd4295c570b31601fbf85908d8d8
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-05-18 15:44:52 -0700

    added style for homepage, all work page and show work page, added work index and show method and views and partials. Added homepage controler for the root path

---
+++ app/controllers/homepages_controller.rb
@@ -0,0 +1,7 @@
+class HomepagesController < ApplicationController
+  def index
+    @movies = Work.where(category: :movie)
+    @books = Work.where(category: :book)
+    @albums = Work.where(category: :album)
+  end
+end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,19 @@
 class WorksController < ApplicationController
   def index
     @works = Work.all
-    @movies = Work.where(category: :movie)
+    @albums = Work.top_works("album")
+    @books = Work.top_works("book")
+    # @books = Work.where(category: :book)
+    @movies = Work.top_works("movie")
+    # @movies = Work.where(category: :movie)
+  end
+
+  def show
+    work_id = params[:id]
+    @work = Work.find_by(id: work_id)
+    if @work.nil?
+      head :not_found
+      return
+    end
   end
 end

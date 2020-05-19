632dd44b70e7854e30e437a7b4e91691db263873
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-05-18 23:26:39 -0700

    most of wave one done - except parts that are related to user or custom model method tests

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -46,6 +46,54 @@ class WorksController < ApplicationController
     end
   end
 
+  def edit
+    # if params[:author_id]
+    #   # This is the nested route, /author/:author_id/books/new
+    #   author = Author.find_by(id: params[:author_id])
+    #   @book = author.books.new
+
+    # else
+      # This is the 'regular' route, /books/new
+      @work = Work.find_by(id: params[:id])
+
+      if @work.nil?
+        head :not_found
+        return
+      end
+    # end
+  end
+
+  def update
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    elsif @work.update(work_params)
+      flash[:success] = "Successfully updated #{@work.category} #{@work.id}"
+      redirect_to work_path(@work.id)
+      return
+    else 
+      flash.now[:error] = "A problem occurred: Could not update #{@work.category}"
+      render :edit, status: :bad_request
+      return
+    end
+  end
+
+  def destroy
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    end
+    flash[:success] = "Successfully destroyed #{@work.category} #{@work.id}"
+    @work.destroy
+    redirect_to root_path
+    return
+  end
+
+  private
+
   def work_params
     return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,67 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe 'validations' do
+    before do
+      # Arrange
+      # author = Author.new(name: 'test author')
+      @work = Work.new(
+        category: "movie",
+        title: 'test work',
+        creator: 'tester',
+        publication_year: 1970,
+        description: 'Incidunt molestias deserunt laudantium.'
+      )
+    end
+
+    it 'is valid when all fields are present' do
+      # Act
+      result = @work.valid?
+
+      # Assert
+      expect(result).must_equal true
+    end
+
+    it 'is invalid without a title' do
+      # Arrange
+      @work.title = nil
+    
+      # Act
+      result = @work.valid?
+    
+      # Assert
+      expect(result).must_equal false
+      expect(@work.errors.messages).must_include :title
+      expect(@work.errors.messages[:title]).must_include "can't be blank"
+    end
+
+    it 'fails validation when the title alreadt exists' do
+      Work.create(title: @work.title)
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :title
+      expect(@work.errors.messages[:title]).must_include "has already been taken"
+    end
+  end
+
+  describe "custom methods" do
+    describe "top works" do
+      it "can pick max 10 top works in each catagory" do
+
+      end
+
+    end
+
+    describe "spotlight" do
+
+      it "can pick the work with maximum votes" do
+
+      end
+      
+      it "if there is a tie with votes, it picks the most recent" do
+
+      end
+
+    end
+
+  end
 end

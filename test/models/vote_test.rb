7e53081d9297253e8633011ea1b5d16364349210
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-05-21 23:32:16 -0700

    initial vote model test

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 describe Vote do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+ 
+  before do
+    @work = works(:work_5)
+    @user = users(:user_2)
+    @vote = Vote.new(
+      {
+        work_id: @work.id,
+        user_id: @user.id
+      }
+    )
+  end
+  
+  it "can be instantiated" do
+    expect(@vote.valid?).must_equal true
+  end
+
+  it "will have the required fields" do
+    @vote.save
+    vote = Vote.last
+    [:user_id, :work_id].each do |field|
+      expect(vote).must_respond_to field
+    end
+  end
 end
